c58826764f0c1f9e73d300da90a7e969d25a845c
Ernesto <eherna65@ucsc.edu>
2020-11-02 16:40:18 -0800

    firebase stuff

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+
+exports.GeoTest = functions.https.onRequest((request, response) =>{
+    // parse request 
+    // request should contian source and destination address
+    let url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    let apikey = functions.config().location.key
+    try{
+        // read body
+        let address = request.body.address;
+        let arr = address.split(' ');
+        
+    } catch (err){
+        // invalid request body
+        functions.logger.error(err);
+        response.status(400).send('invalid json format');
+    }
+})
