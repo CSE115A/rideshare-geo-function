eb38956c853e95ffb40ab437c308d7c82ff80ee5
Ernesto <eherna65@ucsc.edu>
2020-11-03 23:48:03 -0800

    updated functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,33 @@
 const functions = require('firebase-functions');
+const axios = require('axios');
 
-exports.GeoTest = functions.https.onRequest((request, response) =>{
+exports.GeoTest = functions.https.onRequest(async (request, response) =>{
     // parse request 
     // request should contian source and destination address
     let url = 'https://maps.googleapis.com/maps/api/geocode/json'
     let apikey = functions.config().location.key
+    let bodyResp = {}
     try{
-        // read body
         let address = request.body.address;
-        let param = address.replace(' ','+');
-        
+        let param = address.split(' ').join('+');
+        await axios.get(url,{
+            params: {
+                address: address,
+                key: apikey
+            }
+        })
+        .then( response => {
+            bodyResp.geo = response.data.results[0].geometry.location;
+            bodyResp.id = response.data.results[0].place_id;
+        })
+        .catch ( err => {
+            console.log(err);
+        })
+        response.status(200).send(bodyResp);
         
     } catch (err){
         // invalid request body
         functions.logger.error(err);
-        response.status(400).send('invalid json format');
+        response.status(400).send(err);
     }
-})
+});
