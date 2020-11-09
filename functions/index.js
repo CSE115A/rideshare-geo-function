3e309bddfb81adf17da407a1d774a2a9ef10ba2c
Prateek Narayanan <prateeknarayanan@gmail.com>
2020-11-08 18:18:01 -0800

    Modified send in each block and changed function name to getGeo

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
 
-exports.GeoTest = functions.https.onRequest(async (request, response) =>{
+exports.getGeo = functions.https.onRequest(async (request, response) =>{
     // parse request 
     // request should contian source and destination address
     let url = 'https://maps.googleapis.com/maps/api/geocode/json'
@@ -15,18 +15,31 @@ exports.GeoTest = functions.https.onRequest(async (request, response) =>{
                 key: apikey
             }
         })
-        .then( response => {
-            bodyResp.geo = response.data.results[0].geometry.location;
-            bodyResp.id = response.data.results[0].place_id;
+        .then( res => {
+            bodyResp.geo = res.data.results[0].geometry.location;
+            bodyResp.id = res.data.results[0].place_id;
+            return response.status(200).send({
+                error: false,
+                status: 200,
+                message: bodyResp
+            });
         })
         .catch ( err => {
-            console.log(err);
+            functions.logger.error(err);
+            return response.status(500).send({
+                error: true,
+                status: 500,
+                message: "Internal Server Error, try again"
+            });
         })
-        response.status(200).send(bodyResp);
-        
     } catch (err){
         // invalid request body
         functions.logger.error(err);
-        response.status(400).send(err);
+        return response.status(400).send({
+            error: true,
+            status: 400,
+            message: "Params Error: missing or incorrect address format"
+
+        });
     }
 });
