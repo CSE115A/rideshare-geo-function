dfe72f0544fa92e7a311af167d40b7e3394e0bd5
Isai Lopez <ilopezro@ucsc.edu>
2020-11-17 00:34:06 -0800

    refactored index code

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,11 @@ const functions = require("firebase-functions");
 const axios = require("axios");
 
 exports.getGeo = functions.https.onRequest(async (request, response) => {
-  const url = functions.config().location.key;
+  const locationEndpoint = functions.config().location.endpoint;
   const apikey = functions.config().location.key;
   let bodyResp = {};
-  let address;
-  try {
-    address = request.query.address;
-  } catch (error) {
+  const address = request.query.address;
+  if (address === undefined) {
     // missing address as params
     return response.status(400).send({
       error: true,
@@ -16,8 +14,9 @@ exports.getGeo = functions.https.onRequest(async (request, response) => {
       message: "Params Error: missing or incorrect address format",
     });
   }
-  await axios
-    .get(url, {
+
+  return await axios
+    .get(locationEndpoint, {
       params: {
         address,
         key: apikey,
@@ -25,14 +24,12 @@ exports.getGeo = functions.https.onRequest(async (request, response) => {
     })
     .then((resp) => {
       if (resp.data.status === "REQUEST_DENIED") {
-        // invalid api key
-        return response.status(500).send({
+        return response.status(401).send({
           error: true,
-          status: 500,
-          message: "Internal Server Error",
+          status: 401,
+          message: "Invalid API Key!",
         });
       }
-      // valid request: correct address
       bodyResp.geo = resp.data.results[0].geometry.location;
       bodyResp.id = resp.data.results[0].place_id;
       return response.status(200).send({
@@ -41,13 +38,11 @@ exports.getGeo = functions.https.onRequest(async (request, response) => {
         message: bodyResp,
       });
     })
-    .catch(() => {
-      // incorrect address format
-      return response.status(400).send({
+    .catch((err) => {
+      return response.status(500).send({
         error: true,
-        status: 400,
-        message: "Params Error: missing or incorrect address format",
+        status: 500,
+        message: `Internal Server Error: ${err.response.data.error_message}`,
       });
     });
-  return response;
 });
