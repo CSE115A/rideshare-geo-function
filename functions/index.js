ba7d20725607cd4edbae2a5b068f9891a2542153
Isai Lopez <ilopezro@ucsc.edu>
2020-11-17 00:21:07 -0800

    added linter changes

--- functions/index.js
+++ functions/index.js
@@ -1,53 +1,53 @@
-const functions = require('firebase-functions');
-const axios = require('axios');
+const functions = require("firebase-functions");
+const axios = require("axios");
 
-exports.getGeo = functions.https.onRequest(async (request, response) =>{
-    const url = 'https://maps.googleapis.com/maps/api/geocode/json';
-    const apikey = functions.config().location.key;
-    let bodyResp = {};
-    let address;
-    try{
-        address = request.query.address;
-    } catch(error) {
-        // missing address as params
-        return response.status(400).send({
-            error: true,
-            status: 400,
-            message: "Params Error: missing or incorrect address format"
-        });
-    }
-    await axios.get(url,{
-        params: {
-            address,
-            key: apikey
-        }
+exports.getGeo = functions.https.onRequest(async (request, response) => {
+  const url = functions.config().location.key;
+  const apikey = functions.config().location.key;
+  let bodyResp = {};
+  let address;
+  try {
+    address = request.query.address;
+  } catch (error) {
+    // missing address as params
+    return response.status(400).send({
+      error: true,
+      status: 400,
+      message: "Params Error: missing or incorrect address format",
+    });
+  }
+  await axios
+    .get(url, {
+      params: {
+        address,
+        key: apikey,
+      },
     })
-    .then( (resp) => {
-        if (resp.data.status === 'REQUEST_DENIED'){
-            // invalid api key
-            return response.status(500).send({
-                error: true,
-                status: 500,
-                message: "Internal Server Error",
-            });
-        }
-        // valid request: correct address
-        bodyResp.geo = resp.data.results[0].geometry.location;
-        bodyResp.id = resp.data.results[0].place_id;
-        return response.status(200).send({
-            error: false,
-            status: 200,
-            message: bodyResp
+    .then((resp) => {
+      if (resp.data.status === "REQUEST_DENIED") {
+        // invalid api key
+        return response.status(500).send({
+          error: true,
+          status: 500,
+          message: "Internal Server Error",
         });
+      }
+      // valid request: correct address
+      bodyResp.geo = resp.data.results[0].geometry.location;
+      bodyResp.id = resp.data.results[0].place_id;
+      return response.status(200).send({
+        error: false,
+        status: 200,
+        message: bodyResp,
+      });
     })
-    .catch ( (err) => {
-        // incorrect address format
-        console.error(err);
-        return response.status(400).send({
-            error: true,
-            status: 400,
-                message: "Params Error: missing or incorrect address format",
-        });
-    })
-    return response;
+    .catch(() => {
+      // incorrect address format
+      return response.status(400).send({
+        error: true,
+        status: 400,
+        message: "Params Error: missing or incorrect address format",
+      });
+    });
+  return response;
 });
--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -5,58 +5,58 @@ const mockConfig = require("firebase-functions-test")();
 const request = {};
 
 const response = {
-    status: (status) =>{
-        response.statusCode = status;
-        return response;
-    },
-    send: ({ error, status, message }) => {
-        response.body = {
-          error: error,
-          status: status,
-          message: message,
-        };
-        return response;
-    },
-    set: () => {},
+  status: (status) => {
+    response.statusCode = status;
+    return response;
+  },
+  send: ({ error, status, message }) => {
+    response.body = {
+      error: error,
+      status: status,
+      message: message,
+    };
+    return response;
+  },
+  set: () => {},
 };
 
 describe("Calling getGeo with proper params", () => {
-    beforeEach( () => {
-        request.query = {
-            address: '1600 Amphitheatre Parkway, Mountain View, CA'
-        }
-    })
-    it("Returns 200 with proper body", async () => {
-        await getGeo(request,response);
-        expect(response.statusCode).equal(200);
-    });
+  beforeEach(() => {
+    request.query = {
+      address: "1600 Amphitheatre Parkway, Mountain View, CA",
+    };
+  });
+  it("Returns 200 with proper body", async () => {
+    await getGeo(request, response);
+    expect(response.statusCode).equal(200);
+  });
 });
 
 describe("undefined query", () => {
-    beforeEach( () => {
-        request.query = { undefined };
-    })
-    it('returns 400 incorrect fields', async () => {
-        await getGeo(request,response);
-        expect(response.statusCode).equal(400);
-    })
+  beforeEach(() => {
+    request.query = { undefined };
+  });
+  it("returns 400 incorrect fields", async () => {
+    await getGeo(request, response);
+    expect(response.statusCode).equal(400);
+  });
 });
 describe("Missing API key", () => {
-    beforeEach( () => {
-        mockConfig.mockConfig({
-            location: { key: "fake"},
-        });
-        request.query = { address: "1600 Amphitheatre Parkway, Mountain View, CA"};
-    })
-    it('Returns 500 Internal Server Error', async () => {
-        await getGeo(request, response);
-        expect(response.statusCode).equal(500);
-    })
+  beforeEach(() => {
+    mockConfig.mockConfig({
+      location: { key: "fake" },
+    });
+    request.query = { address: "1600 Amphitheatre Parkway, Mountain View, CA" };
+  });
+  it("Returns 500 Internal Server Error", async () => {
+    await getGeo(request, response);
+    expect(response.statusCode).equal(500);
+  });
 });
 
 describe("Missing Query", () => {
-    it('Returns 400 improper request', async () => {
-        await getGeo({},response);
-        expect(response.statusCode).equal(400);
-    })
-})
+  it("Returns 400 improper request", async () => {
+    await getGeo({}, response);
+    expect(response.statusCode).equal(400);
+  });
+});
